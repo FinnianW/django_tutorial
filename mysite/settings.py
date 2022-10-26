60d85dc773d11c161bbd76e07e1deea0b73d6958
Finn Wengerd <finnian.wengerd@outlook.com>
2022-10-25 20:59:39 -0400

    Securing private key

--- mysite/settings.py
+++ mysite/settings.py
@@ -18,10 +18,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^+%9wqyex@cm88cnfdzt(ixw1u7!(z^%0)@#%tcweoc7pqwda$'
-
+# SECRET KEY REMOVED
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
